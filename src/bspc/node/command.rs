cae1954273457c0905312c02df417c663dc21c22
BenKenobi <villauben@gmail.com>
2024-09-05 00:39:22 +0200

    node commands now generally take a node selector like the bspc command

--- src/bspc/node/command.rs
+++ src/bspc/node/command.rs
@@ -1,6 +1,6 @@
 use crate::bspc::node::state as NodeState;
-use crate::bspc::node::flags as NodeFlag;
-use crate::bspc::node::layers as NodeLayer;
+use crate::bspc::node::flag as NodeFlag;
+use crate::bspc::node::layer as NodeLayer;
 use crate::bspc::node::resize_pos as ResizePos;
 
 use crate::bspc::desktop::selection as DesktopSelection;
@@ -11,36 +11,105 @@ use crate::socket_communication::send_message;
 use DesktopSelection::DesktopSelector;
 use MonitorSelection::MonitorSelector;
 
-use super::directions::Direction;
+use super::direction::Direction;
 use super::selector::NodeSelector;
 
+/// Commands that act on nodes
 pub enum NodeCommand {
+    /// Focuses the selected node.
+    /// # Arguments
+    /// - `node_sel`: The node to focus
     Focus(NodeSelector),
+    /// Activates the selected node.
+    /// # Arguments
+    /// - `node_sel`: The node to activate
     Activate(NodeSelector),
-    ToDesktop(DesktopSelector, bool),
-    ToMonitor(MonitorSelector, bool),
-    ToNode(NodeSelector, bool),
-    Swap(NodeSelector),
-    PreselectDirection(Direction),
-    Move(i32, i32),
-    Resize(ResizePos::ResizePos, i32, i32),
-    Ratio(f64),
+    /// Moves the selected node to the selected desktop.
+    /// # Arguments
+    /// - `node_sel`: The node to move
+    /// - `desktop_sel`: The desktop to move the node to
+    /// - `follow`: True if the focus should follow the moved node
+    ToDesktop(NodeSelector, DesktopSelector, bool),
+    /// Moves the selected node to the selected monitor.
+    /// # Arguments
+    /// - `node_sel`: The node to move
+    /// - `monitor_sel`: The monitor to move the node to
+    /// - `follow`: True if the focus should follow the moved node
+    ToMonitor(NodeSelector, MonitorSelector, bool),
+    /// Moves the first selected node to the second selected node.
+    /// # Arguments
+    /// - `node_sel_a`: The node to move
+    /// - `node_sel_b`: The node to move the first node to
+    /// - `follow`: True if the focus should follow the moved node
+    ToNode(NodeSelector, NodeSelector, bool),
+    /// Swaps the two selected nodes.
+    /// # Arguments
+    /// - `node_sel_a`: One of the nodes to swap
+    /// - `node_sel_b`: The other of the nodes to swap
+    Swap(NodeSelector, NodeSelector),
+    //TODO cancel
+    /// Starts preselection in the selected node in the given direction.
+    /// #Arguments
+    /// - `node_sel`: The node to split
+    /// - `direction`: The direction to preselect in
+    PreselectDirection(NodeSelector, Direction),
+    /// Moves the selected node by the given amount and direction.
+    /// # Arguments
+    /// - `node_sel`: The node to move
+    /// - `dx`: How many pixels to move the window to the right
+    /// - `dy`: How many pixels to move the window downwards
+    Move(NodeSelector, i32, i32),
+    /// Rezises the selected node from the given position by the given values.
+    /// # Arguments
+    /// - `node_sel`: The node to resize
+    /// - `resize_pos`: The position to start resizing from
+    /// - `dx`: How much to resize to the right
+    /// - `dx`: How much to resize downwards
+    Resize(NodeSelector, ResizePos::ResizePos, i32, i32),
+    /// Sets the splitting ratio of the selected node.
+    /// # Arguments
+    /// - `node_sel`: The node to set the splitting ratio for.
+    /// - `ratio`: New splitting ratio (0,1)
+    Ratio(NodeSelector, f64),
     // TODO rotate
     // TODO flip
-    Equalize,
-    Balance,
+    /// Resets the splitting ratios of the tree rooted at the selected node.
+    /// # Arguments
+    /// - `node_sel`: The node that is the root of the tree to equalize
+    Equalize(NodeSelector),
+    /// Adjusts splitting ratios so that all windows in the tree rooted in the selected node occupy the same area.
+    /// # Arguments
+    /// - `node_sel`: The node that is the root of the tree to balance
+    Balance(NodeSelector),
     // TODO circulate
-    State(NodeState::NodeState),
-    Flag(NodeFlag::NodeFlag),
-    Layer(NodeLayer::NodeLayer),
+    /// Sets the state of the selected node.
+    /// # Arguments
+    /// - `node_sel`: The node to set the state for
+    /// - `state`: New flag of the focused node
+    State(NodeSelector, NodeState::NodeState),
+    /// Sets/toggles flag for the selected node.
+    /// # Arguments
+    /// - `node_sel`: The node to set/toggle the flag for
+    /// - `flag`: The flag to set/toggle
+    Flag(NodeSelector, NodeFlag::NodeFlag),
+    /// Sets the layer for the selected node.
+    /// # Arguments
+    /// - `node_sel`: The node to set the layer for
+    /// - `layer`: The layer to set
+    Layer(NodeSelector, NodeLayer::NodeLayer),
     InsertReceptacle,
-    Close,
-    Kill
+    /// Closes the selected node.
+    /// # Arguments
+    /// - `node_sel`: The root of the tree to close all windows in
+    Close(NodeSelector),
+    /// Kills the selected node.
+    /// # Arguments
+    /// - `node_sel`: The root of the tree to kill all windows in
+    Kill(NodeSelector),
 }
 
 impl NodeCommand {
     pub fn get_response(&self) -> Option<Vec<String>> {
-        println!("sending message: {:?}", self.assemble());
         send_message(get_bspc_socket_path(), self.assemble())
     }
 
@@ -56,75 +125,90 @@ impl NodeCommand {
                 result.push(String::from("--activate"));
                 result.push(node_sel.assemble());
             }
-            NodeCommand::ToDesktop(desktop_sel, follow) => {
+            NodeCommand::ToDesktop(node_sel, desktop_sel, follow) => {
+                result.push(node_sel.assemble());
                 result.push(String::from("--to-desktop"));
                 result.push(desktop_sel.assemble());
                 if *follow {
                     result.push(String::from("--follow"));
                 }
             }
-            NodeCommand::ToMonitor(monitor_sel, follow) => {
+            NodeCommand::ToMonitor(node_sel, monitor_sel, follow) => {
+                result.push(node_sel.assemble());
                 result.push(String::from("--to-monitor"));
                 result.push(monitor_sel.assemble());
                 if *follow {
                     result.push(String::from("--follow"));
                 }
             }
-            NodeCommand::ToNode(node_sel, follow) => {
+            NodeCommand::ToNode(node_sel1, node_sel2, follow) => {
+                result.push(node_sel1.assemble());
                 result.push(String::from("--to-node"));
-                result.push(node_sel.assemble());
+                result.push(node_sel2.assemble());
                 if *follow {
                     result.push(String::from("--follow"));
                 }
             }
-            NodeCommand::Swap(node_sel) => {
+            NodeCommand::Swap(node_sel1, node_sel2) => {
+                result.push(node_sel1.assemble());
                 result.push(String::from("--swap"));
-                result.push(node_sel.assemble());
+                result.push(node_sel2.assemble());
             }
-            NodeCommand::PreselectDirection(direction) => {
+            NodeCommand::PreselectDirection(node_sel, direction) => {
+                result.push(node_sel.assemble());
                 result.push(String::from("--presel-dir"));
                 result.push(direction.get_string());
             }
-            NodeCommand::Move(x, y) => {
+            NodeCommand::Move(node_sel, x, y) => {
+                result.push(node_sel.assemble());
                 result.push(String::from("--move"));
                 result.push(x.to_string());
                 result.push(y.to_string());
             }
-            NodeCommand::Resize(resize_pos, x, y) => {
+            NodeCommand::Resize(node_sel, resize_pos, x, y) => {
+                result.push(node_sel.assemble());
                 result.push(String::from("--resize"));
                 result.push(ResizePos::get_string(resize_pos));
                 result.push(x.to_string());
                 result.push(y.to_string());
             }
-            NodeCommand::Ratio(ratio) => {
+            NodeCommand::Ratio(node_sel, ratio) => {
+                result.push(node_sel.assemble());
                 result.push(String::from("--ratio"));
                 result.push(ratio.to_string());
             }
-            NodeCommand::Equalize => {
+            NodeCommand::Equalize(node_sel) => {
+                result.push(node_sel.assemble());
                 result.push(String::from("--equalize"));
             }
-            NodeCommand::Balance => {
+            NodeCommand::Balance(node_sel) => {
+                result.push(node_sel.assemble());
                 result.push(String::from("--balance"));
             }
-            NodeCommand::State(state) => {
+            NodeCommand::State(node_sel, state) => {
+                result.push(node_sel.assemble());
                 result.push(String::from("--state"));
                 result.push(NodeState::get_string(state));
             }
-            NodeCommand::Flag(flag) => {
+            NodeCommand::Flag(node_sel, flag) => {
+                result.push(node_sel.assemble());
                 result.push(String::from("--flag"));
                 result.push(NodeFlag::get_string(flag));
             }
-            NodeCommand::Layer(layer) => {
+            NodeCommand::Layer(node_sel, layer) => {
+                result.push(node_sel.assemble());
                 result.push(String::from("--layer"));
                 result.push(NodeLayer::get_string(layer));
             }
             NodeCommand::InsertReceptacle => {
                 result.push(String::from("--insert-receptacle"));
             }
-            NodeCommand::Close => {
+            NodeCommand::Close(node_sel) => {
+                result.push(node_sel.assemble());
                 result.push(String::from("--close"));
             }
-            NodeCommand::Kill => {
+            NodeCommand::Kill(node_sel) => {
+                result.push(node_sel.assemble());
                 result.push(String::from("--kill"));
             }
         }
