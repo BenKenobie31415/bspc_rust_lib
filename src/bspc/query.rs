d9c10db2a6783b048d6dc8c29e90486d582d1825
BenKenobi <villauben@gmail.com>
2024-09-05 00:40:03 +0200

    added concise versions of query commands

--- src/bspc/query.rs
+++ src/bspc/query.rs
@@ -7,15 +7,42 @@ use NodeSelection::NodeSelector;
 use DesktopSelection::DesktopSelector;
 use MonitorSelection::MonitorSelector;
 
+
+/// Enum containing the different bspc commands to query information about the state of bspwm.
+/// Generally acts like the bspc-query command concerning defaults and other behaviour.
 pub enum QueryCommand {
+    /// # Arguments
+    /// - `node_sel`: The `NodeSelector`
+    /// - `desktop_sel`: The `DesktopSelector`
+    /// - `monitor_sel`: The `MonitorSelector`
     Nodes(Option<NodeSelector>, Option<DesktopSelector>, Option<MonitorSelector>),
+    /// Concise version of the `Nodes` query.
+    /// # Arguments
+    /// - `node_sel`: The `NodeSelector`
+    CNodes(Option<NodeSelector>),
+    /// # Arguments
+    /// - `node_sel`: The `NodeSelector`
+    /// - `desktop_sel`: The `DesktopSelector`
+    /// - `monitor_sel`: The `MonitorSelector`
     Desktops(Option<NodeSelector>, Option<DesktopSelector>, Option<MonitorSelector>, bool),
+    ///Concise version of the `Desktops` query.
+    /// # Arguments
+    /// - `desktop_sel`: The `DesktopSelector`
+    CDesktops(Option<DesktopSelector>, bool),
+    /// # Arguments
+    /// - `node_sel`: The `NodeSelector`
+    /// - `desktop_sel`: The `DesktopSelector`
+    /// - `monitor_sel`: The `MonitorSelector`
     Monitors(Option<NodeSelector>, Option<DesktopSelector>, Option<MonitorSelector>, bool),
+    ///Concise version of the `Monitors` query.
+    /// # Arguments
+    /// - `monitor_sel`: The `MonitorSelector`
+    CMonitors(Option<MonitorSelector>, bool),
     Tree(Option<NodeSelector>, Option<DesktopSelector>, Option<MonitorSelector>),
 }
 
 impl QueryCommand {
-    pub(crate) fn assemble(&self) -> Vec<String> {
+    fn assemble(&self) -> Vec<String> {
         let mut result: Vec<String> = Vec::new();
         result.push(String::from("query"));
         match self {
@@ -25,11 +52,22 @@ impl QueryCommand {
                 push_desktop_selector(&mut result, desktop_sel);
                 push_monitor_selector(&mut result, monitor_sel);
             }
+            QueryCommand::CNodes(node_sel) => {
+                result.push(String::from("--nodes"));
+                push_node_selector(&mut result, node_sel);
+            }
             QueryCommand::Desktops(node_sel, desktop_sel, monitor_sel, names) => {
                 result.push(String::from("--desktops"));
                 push_node_selector(&mut result, node_sel);
                 push_desktop_selector(&mut result, desktop_sel);
                 push_monitor_selector(&mut result, monitor_sel);
+                if *names {
+                    result.push(String::from("--names"));
+                }
+            }
+            QueryCommand::CDesktops(desktop_sel, names) => {
+                result.push(String::from("--desktops"));
+                push_desktop_selector(&mut result, desktop_sel);
                 if *names {
                     result.push(String::from("--names"));
                 }
@@ -43,6 +81,13 @@ impl QueryCommand {
                     result.push(String::from("--names"));
                 }
             }
+            QueryCommand::CMonitors(monitor_sel, names) => {
+                result.push(String::from("--monitors"));
+                push_monitor_selector(&mut result, monitor_sel);
+                if *names {
+                    result.push(String::from("--names"));
+                }
+            }
             QueryCommand::Tree(node_sel, desktop_sel, monitor_sel) => {
                 result.push(String::from("--tree"));
                 push_node_selector(&mut result, node_sel);
@@ -50,10 +95,10 @@ impl QueryCommand {
                 push_monitor_selector(&mut result, monitor_sel);
             }
         }
-        // println!("query command: {:?}", result);
         result
     }
 
+    /// Executes the command and returns the result of the query returned by bspc
     pub fn get_response(&self) -> Option<Vec<String>> {
         match send_message(get_bspc_socket_path(), self.assemble()) {
             Some(message) => {
