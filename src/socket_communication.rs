925818a7ed1fec97db23c2bc9785d0a16f7f511e
BenKenobi <villauben@gmail.com>
2024-09-10 00:39:57 +0200

    added count support for event subscription

--- src/socket_communication.rs
+++ src/socket_communication.rs
@@ -2,20 +2,19 @@ use std::os::unix::net::UnixStream;
 use std::io::prelude::*;
 
 pub(crate) fn send_message(command: Vec<String>) -> Option<Vec<String>> {
-    let socket_path = get_bspc_socket_path();
     //println!("sending message: {:?}", command);
-    let mut client = UnixStream::connect(socket_path).expect("Failed to connect to socket");
 
-    let command_string = format!("{}\x00", command.join("\x00"));
+    let command_string = assemble_message(command);
 
+    let mut client = get_client();
     client
         .write_all(command_string.as_bytes())
-        .expect("Failed to send command");
+        .expect("should always be able to write to bspwm socket");
 
     let mut response = Vec::new();
     client
         .read_to_end(&mut response)
-        .expect("Failed to load response");
+        .expect("should always get a response from bspwm socket");
 
     if !response.is_empty() {
         let response_string = String::from_utf8_lossy(&response);
@@ -29,7 +28,15 @@ pub(crate) fn send_message(command: Vec<String>) -> Option<Vec<String>> {
     }
 }
 
-fn get_bspc_socket_path() -> String {
+pub(crate) fn assemble_message(command: Vec<String>) -> String {
+    format!("{}\x00", command.join("\x00"))
+}
+
+pub(crate) fn get_client() -> UnixStream {
+    UnixStream::connect(get_bspwm_socket_path()).expect("bspwm socket should always be available")
+}
+
+fn get_bspwm_socket_path() -> String {
     let socket_path = String::from("/tmp/bspwm_0_0-socket");
     match std::env::var("BSPWM_SOCKET") {
         Ok(path) => {
--- src/subscription.rs
+++ src/subscription.rs
@@ -1,24 +1,11 @@
-//! Package managing subscription to bspc-events
+//! Package managing subscription to bspwm-events
 
 use std::thread;
-use std::env;
-use std::os::unix::net::UnixStream;
 use std::io::prelude::*;
 use std::thread::JoinHandle;
 
-use crate::bspc::events::Event;
+use crate::{bspc::events::Event, socket_communication::{assemble_message, get_client}};
 
-/// Struct for managing eventcallbacks
-/// # Example
-/// ```
-/// let subscription_handler = SubscriptionHandler::new();
-/// subscription_handler.subscribe(Event::NodeAdd, callback, 73);
-/// subscription_handler.await_threads();
-///
-/// fn callback(payload: Vec<&str>, callback_args: &i32) {
-///     println!("Received event payload {:?} together with additional arguments {}", payload, callback_args);
-/// }
-/// ```
 pub struct SubscriptionHandler {
     thread_handles: Vec<JoinHandle<()>>,
 }
@@ -31,29 +18,8 @@ impl SubscriptionHandler {
         }
     }
 
-    /// Tells the SubscriptionHandler to call the function `callback` when the `event` gets triggered by bspc.
-    ///
-    /// # Arguments
-    /// - `event`: The bspc event which should trigger the callback
-    /// - `callback`: The function to be called when the bspc event gets fired.
-    /// The callback needs to take a `Vec<&str>` as arguments which will be the payload of the event. It also has to take '&T' which will be given the `callback_args`.
-    /// - `callback_args`: Additional arguments that can be passed to the callback for more flexibility
-    ///
-    /// # Panics
-    /// Only panics when the bspc socket is not available and/or there are issues writing/reading from it
-    ///
-    /// # Example
-    /// ```
-    /// let subscription_handler = SubscriptionHandler::new();
-    /// subscription_handler.subscribe(Event::NodeAdd, callback, 73);
-    /// subscription_handler.await_threads();
-    ///
-    /// fn callback(payload: Vec<&str>, callback_args: &i32) {
-    ///     println!("Received event payload {:?} together with additional arguments {}", payload, callback_args);
-    /// }
-    /// ```
-    pub fn subscribe<T:Send + 'static>(&mut self, event: Event, callback: fn(Vec<&str>, &T), callback_args: T) {
-        self.thread_handles.push(subscribe::<T>(event, callback, callback_args));
+    pub fn subscribe<T:Send + 'static>(&mut self, event: Event, callback: fn(Vec<&str>, &T), callback_args: T, count: i32) {
+        self.thread_handles.push(add_subscriber(event, callback, callback_args, count));
     }
 
     /// Blocks and manages the subscriptions
@@ -64,34 +30,31 @@ impl SubscriptionHandler {
     }
 }
 
-fn subscribe<T:Send + 'static>(event: Event, callback: fn(Vec<&str>, &T), callback_args: T) -> JoinHandle<()> {
-    let handle = thread::spawn(move || {
-        add_subscriber::<T>(event, callback, callback_args);
-    });
-    handle
-}
+fn add_subscriber<T:Send + 'static>(event: Event, callback: fn(Vec<&str>, &T), callback_args: T, count: i32) -> JoinHandle<()> {
+    thread::spawn(move || {
+        let mut client = get_client();
 
-fn add_subscriber<T>(event: Event, callback: fn(Vec<&str>, &T), callback_args: T) {
-    let socket_path = match env::var("BSPWM_SOCKET") {
-        Ok(val) => val,
-        Err(_e) => String::from("/tmp/bspwm_0_0-socket"),
-    };
-    let mut client = UnixStream::connect(socket_path).expect("should be able to connect to socket");
-
-    let subscribe_message = format!("subscribe\x00{}\x00", event.get_str());
-    client
-        .write_all(subscribe_message.as_bytes())
-        .expect("should be able to write subscription message to the socket");
-
-    loop {
-        let mut buffer = [0; 4096];
-        let bytes_read = client
-            .read(&mut buffer)
-            .expect("socket should alway be able to be read");
-        if bytes_read > 0 {
-            let data = String::from_utf8_lossy(&buffer[..bytes_read]);
-            let payload: Vec<&str> = data.trim_end_matches('\n').split(' ').skip(1).collect();
-            callback(payload, &callback_args);
+        let mut command = vec!["subscribe".to_string(), event.get_str()];
+        if count > 0 {
+            command.push("--count".to_string());
+            command.push(count.to_string());
         }
-    }
+        let subscription_message = assemble_message(command);
+
+        client
+            .write_all(subscription_message.as_bytes())
+            .expect("should always be able to write to bspwm socket");
+
+        loop {
+            let mut buffer = [0; 4096];
+            let bytes_read = client
+                .read(&mut buffer)
+                .expect("should always be able to read bspwm socket");
+            if bytes_read > 0 {
+                let data = String::from_utf8_lossy(&buffer[..bytes_read]);
+                let payload: Vec<&str> = data.trim_end_matches('\n').split(' ').skip(1).collect();
+                callback(payload, &callback_args);
+            }
+        }
+    })
 }
